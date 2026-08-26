c528eb33a53a1a75b5f53469245a1570e8a5c0cc
DoctorSoup143 <sdrobish4@gmail.com>
2026-08-26 18:51:57 -0400

    Add empty FluentStyle resource dictionary, merged into App.xaml

---
+++ SidebarDiagnostics/FluentStyle.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace SidebarDiagnostics.Style
+{
+	public partial class FluentStyle : ResourceDictionary
+	{
+		public FluentStyle()
+		{
+			InitializeComponent();
+		}
+	}
+}
